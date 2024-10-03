5ac01b9deb8757072fe5de83fb345b3cbbfe57c0
Nbtmadhu <128582144+Nbtmadhu@users.noreply.github.com>
2024-10-03 12:32:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 // Put Your Session Id Here
-SESSION_ID: process.env.SESSION_ID || "xzNU2QLJ#dUFKBKM-LgBxn-aoZpT8wgyvCXsVELWTyCVw6vh6Tlo",
+SESSION_ID: process.env.SESSION_ID || "fdsXUZ4B#3kQqMUKlhufsaEwkT8QWPiVYyjAyD4iMRquJuLqw0DY",
 OWNER_NUMBER: '94760059126@s.whatsapp.net',
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.postimg.cc/xdhMJy1b/Screenshot-20240830-004711-Pixel-Lab.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi QUEEN-KYLIE-MD Is Online Now 💻\n*💻 Owner* - QUEEN-KYLIE-MD\n\n*💻 Owner Number* -94760059126\n\n_විධාන මෙනුව ලබා ගැනීමට .menu ලෙස ටයිප් කරන්න._",
